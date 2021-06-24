ba8de24aa364e78b849b2f6797ccdadce29639a9
KailaneCunha <86396415+KailaneCunha@users.noreply.github.com>
2021-06-24 02:53:25 -0300

    Create figura.cs

---
+++ figura.cs
@@ -0,0 +1,20 @@
+namespace  Aula10
+{
+public  class  Figura
+{
+    protegido  _area duplo  ; // atributo
+     _perimetro duplo  protegido ; // atributo
+
+    public double Area // propriedade
+    {
+        // leitura do atributo correspondente
+        get {return this._area; }
+    }
+
+    público  duplo  Perimetro  // propriedade
+    {
+        // leitura do atributo correspondente
+        get { return  this . _perimetro ; }
+    }
+}
+}
